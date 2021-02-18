077d95d0685c981709b9f9598816b3cfc167cdde
CaoMeiYouRen <996881204@qq.com>
2021-02-19 00:11:11 +0800

    feat: 完成模板

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import { createVuePlugin } from 'vite-plugin-vue2'
+import path from 'path'
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [createVuePlugin({})],
+    server: {
+        port: 3000,
+        open: true,
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+})
