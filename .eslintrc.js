d6eae28efaf441553618e7752cf3b498a10acd5a
CaoMeiYouRen <996881204@qq.com>
2021-03-01 20:51:08 +0800

    ci: add test.yml

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
-const IS_PROD = process.env.NODE_ENV === 'production'
+// const IS_PROD = process.env.NODE_ENV === 'production'
 module.exports = {
     root: true,
     extends: [
-        'cmyr/vue3',
+        'cmyr/vue',
     ],
     rules: {
     },
