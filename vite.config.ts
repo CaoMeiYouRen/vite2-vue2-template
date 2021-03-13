4bc1664c4ad009dc22b83f77174087f3dc12d6e2
CaoMeiYouRen <996881204@qq.com>
2021-03-14 00:12:19 +0800

    build: 修复路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         createVuePlugin({}),
-        tsconfigPaths({}),
     ],
     server: {
         port: 3000,
         open: true,
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
     },
 })
